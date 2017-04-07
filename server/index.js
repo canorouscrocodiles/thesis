29b36cca427e7682f9ac46be7a7a4335af94b335
Craig Rodrigues <PagingCraig@gmail.com>
2017-04-07 14:40:42 -0700

    Complete simple server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(express.static(path.join(__dirname, '../public/')))
+app.listen(8080, () => console.log('Listening on port 8080'))
