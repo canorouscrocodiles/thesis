caee5e2be8072eccc57a7d0489cf9ea1b9e91925
Innie Kim <innie.jong.kim@gmail.com>
2017-04-09 15:50:40 -0700

    (fix) Add reference to process.env.DBNAME - Also added console.logs of host, port, user, and database name for easier debugging

--- server/db/index.js
+++ server/db/index.js
@@ -7,5 +7,8 @@ const pgp = require('pg-promise')(options)
 const db = pgp(project.db)
 
 console.log(`DB Host: ${project.db.host}`)
+console.log(`DB Port: ${project.db.port}`)
+console.log(`DB User: ${project.db.user}`)
+console.log(`DB Name: ${project.db.database}`)
 
 module.exports = db
