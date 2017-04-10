980d622a1246020707da56c256558efad7d492e0
Craig Rodrigues <PagingCraig@gmail.com>
2017-04-10 11:21:52 -0700

    Update test to use process.env

--- server/sockets/sockets.test.js
+++ server/sockets/sockets.test.js
@@ -1,10 +1,12 @@
 const socket = require('socket.io-client')
 const { io, server } = require('../index')
+const url = process.env.HOST || 'http://localhost'
+const port = process.env.PORT || 8080
 
 describe('Socket Tests', () => {
   it('Should emit Hello World', done => {
-    server.listen(8080)
-    let client = socket.connect('http://localhost:8080')
+    server.listen(port)
+    let client = socket.connect(`${url}:${port}`)
     client.on('connect', () => {
       client.on('echo', data => {
         expect(data).toEqual('Hello World')
@@ -16,8 +18,8 @@ describe('Socket Tests', () => {
     })
   })
   it('Should emit one client', done => {
-    server.listen(8080)
-    let client = socket.connect('http://localhost:8080')
+    server.listen(port)
+    let client = socket.connect(`${url}:${port}`)
     client.on('connect', () => {
       client.on('clients', data => {
         let result = Object.keys(data)
