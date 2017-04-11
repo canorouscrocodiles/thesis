61858c1308e991db1eef95a6653ea399dfb2bc6a
Keith Alpichi <keithalpichi@gmail.com>
2017-04-11 14:39:40 -0700

    fix tests and DB queries to work with new seed data

--- server/db/models/models.test.js
+++ server/db/models/models.test.js
@@ -3,70 +3,15 @@ const answerQueries = require('./answers')
 const questionQueries = require('./questions')
 const userQueries = require('./users')
 const voteQueries = require('./votes')
+const testUtils = require('../testUtils')
+const truncateAndSeed = testUtils.truncateAndSeed
+const createTables = testUtils.createTables
 
-beforeAll(() => createTables())
-
-afterAll(() => truncateAndSeed())
-
-const createTables = () => {
-  return db.none(`CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    created_timestamp TIMESTAMP DEFAULT now() NOT NULL,
-    username VARCHAR (50) NOT NULL UNIQUE,
-    email VARCHAR (50) NOT NULL,
-    img_url VARCHAR (100),
-    bio VARCHAR (100)
-  );`)
-  .then(() => db.none(`CREATE TABLE IF NOT EXISTS questions (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
-    created_timestamp TIMESTAMP DEFAULT now() NOT NULL,
-    updated_timestamp TIMESTAMP DEFAULT now() NOT NULL,
-    message VARCHAR (300) NOT NULL,
-    coordinates VARCHAR (100) NOT NULL,
-    location VARCHAR (100) NOT NULL
-    );`))
-  .then(() => db.none(`CREATE TABLE IF NOT EXISTS answers (
-    id SERIAL PRIMARY KEY,
-    created_timestamp TIMESTAMP DEFAULT now() NOT NULL,
-    message VARCHAR (300) NOT NULL,
-    user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
-    question_id INTEGER NOT NULL REFERENCES questions ON DELETE CASCADE,
-    vote_count INTEGER DEFAULT 0
-    );`))
-  .then(() => db.none(`CREATE TABLE IF NOT EXISTS votes (
-    id SERIAL PRIMARY KEY,
-    created_timestamp TIMESTAMP DEFAULT now() NOT NULL,
-    user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
-    answer_id INTEGER NOT NULL REFERENCES answers ON DELETE CASCADE,
-    vote_type INTEGER DEFAULT 0
-    );`))
-  .then(() => db.none(`CREATE TABLE IF NOT EXISTS categories (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(24) NOT NULL,
-    question_id INTEGER NOT NULL REFERENCES questions ON DELETE CASCADE
-    );`))
-}
-
-const truncateAndSeed = () => {
-  return db.none('TRUNCATE users RESTART IDENTITY CASCADE')
-  .then(() => db.none('TRUNCATE questions RESTART IDENTITY CASCADE'))
-  .then(() => db.none('TRUNCATE answers RESTART IDENTITY CASCADE'))
-  .then(() => db.none('TRUNCATE votes RESTART IDENTITY CASCADE'))
-  .then(() => db.none('TRUNCATE categories RESTART IDENTITY CASCADE'))
-  .then(() => db.none('INSERT INTO users (username, email, img_url, bio) VALUES ($1, $2, $3, $4)', ['JohnSmith', 'js@example.com', 'www.example.com/avatar.png', 'My first profile']))
-  .then(() => db.none('INSERT INTO questions (user_id, message, coordinates, location) VALUES ($1, $2, $3, $4)', [1, 'Is Chipotle still open?', '{lat: 82.61723989, lng: -122.9553215}', '123 St. Johns Drive, San Francisco, Ca 90000']))
-  .then(() => db.none('INSERT INTO answers (user_id, question_id, message) VALUES ($1, $2, $3)', [1, 1, 'Yes, they always are open']))
-  .then(() => db.none('INSERT INTO votes (user_id, answer_id, vote_type) VALUES ($1, $2, $3)', [1, 1, 1]))
-  .then(() => db.none('INSERT INTO categories (name, question_id) VALUES ($1, $2)', ['chipotle', 1]))
-  .catch((err) => {
-    console.log('beforeEach failed with error: ', err)
-    throw err
-  })
-}
+beforeAll(() => createTables(db))
+afterAll(() => truncateAndSeed(db))
 
 describe('Database user queries ', () => {
-  beforeEach(() => truncateAndSeed())
+  beforeEach(() => truncateAndSeed(db))
 
   it('selects a user', () => {
     return userQueries.selectUser(1)
@@ -99,7 +44,7 @@ describe('Database user queries ', () => {
 })
 
 describe('Database question queries', () => {
-  beforeEach(() => truncateAndSeed())
+  beforeEach(() => truncateAndSeed(db))
 
   it('selects all questions', () => {
     return questionQueries.selectQuestions()
@@ -114,7 +59,7 @@ describe('Database question queries', () => {
   })
 
   it('inserts a single question', () => {
-    const question = {user_id: 1, message: 'What is the weather like outside?', coordinates: '{lat: 82.61723989, lng: -122.9553215}', location: '123 St. Johns Drive, San Francisco, Ca 90000'}
+    const question = {user_id: 1, message: 'What is the weather like outside?', coordinates: '{lat: 82.61723989, lng: -122.9553215}', location: '123 St. Johns Drive, San Francisco, Ca 90000', category_id: 1}
     return questionQueries.insertQuestion(question)
     .then(() => db.one('SELECT * FROM questions WHERE id = 2'))
     .then(result => expect(result.message).toBe('What is the weather like outside?'))
@@ -122,7 +67,7 @@ describe('Database question queries', () => {
   })
 
   it('updates a single question', () => {
-    const question = {user_id: 1, message: 'What is the weather like outside near the corner of...?', coordinates: '{lat: 82.61723989, lng: -122.9553215}', location: '123 St. Johns Drive, San Francisco, Ca 90000'}
+    const question = {user_id: 1, message: 'What is the weather like outside near the corner of...?', coordinates: '{lat: 82.61723989, lng: -122.9553215}', location: '123 St. Johns Drive, San Francisco, Ca 90000', category_id: 1}
     return questionQueries.updateQuestion(question)
     .then(() => db.one('SELECT * FROM questions WHERE id = 1'))
     .then(result => expect(result.message).toBe('What is the weather like outside near the corner of...?'))
@@ -138,7 +83,7 @@ describe('Database question queries', () => {
 })
 
 describe('Database answer queries', () => {
-  beforeEach(() => truncateAndSeed())
+  beforeEach(() => truncateAndSeed(db))
 
   it('selects all users answers', () => {
     return answerQueries.selectAnswers(1)
@@ -171,7 +116,7 @@ describe('Database answer queries', () => {
 })
 
 describe('Database vote queries', () => {
-  beforeEach(() => truncateAndSeed())
+  beforeEach(() => truncateAndSeed(db))
 
   it('updates a vote', () => {
     const vote = {id: 1, vote_type: -1, answer_id: 1}
--- server/db/models/questions.js
+++ server/db/models/questions.js
@@ -4,7 +4,7 @@ const selectQuestions = () => db.manyOrNone('SELECT * from questions')
 
 const selectQuestion = (id) => db.oneOrNone('SELECT * from questions where id = $1', [id])
 
-const insertQuestion = ({ user_id, message, coordinates, location }) => db.none('INSERT INTO questions (user_id, message, coordinates, location) VALUES ($1, $2, $3, $4)', [user_id, message, coordinates, location])
+const insertQuestion = ({ user_id, message, coordinates, location, category_id }) => db.none('INSERT INTO questions (user_id, message, coordinates, location, category_id) VALUES ($1, $2, $3, $4, $5)', [user_id, message, coordinates, location, category_id])
 
 const updateQuestion = ({ user_id, message, coordinates, location }) => db.none('UPDATE questions SET message = $1, coordinates = $2, location = $3 WHERE user_id = $4', [message, coordinates, location, user_id])
 
