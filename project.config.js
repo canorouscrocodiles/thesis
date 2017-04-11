4a60ac07846eb468b2470673c19ef2eb725af1c0
Craig Rodrigues <PagingCraig@gmail.com>
2017-04-11 14:37:56 -0700

    Remove project.config.js from git

--- project.config.js
+++
@@ -1,5 +0,0 @@
-const projectName = process.env.NAME || 'thesis'
-const env = process.env.ENV || 'test'
-const dbHost = process.env.DATABASE_URL || `postgres://JongK:@localhost:5432/${projectName}_${env}`
-
-module.exports = dbHost
