8536466f8ebfde33d8dd23796bcf4ad9ff8551e0
Keith Alpichi <keithalpichi@gmail.com>
2017-04-10 18:16:56 -0700

    fix routing issue- add catch-all route for all GET requests to send index.html

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ if (!module.parent) {
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../public/')))
 app.use('/api', router)
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')))
 
 io.on('connection', socket)
 
