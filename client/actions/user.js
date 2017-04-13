4b9f12716b9ee400bf61a8fbfa62526dea5a4a7c
Innie Kim <innie.jong.kim@gmail.com>
2017-04-12 17:20:25 -0700

    (feat) Complete User actions, User reducers, Location actions, Location reducers

--- client/actions/user.js
+++ client/actions/user.js
@@ -1,34 +1,39 @@
 import axios from 'axios'
 
-const fetchingUser = (bool) => {
+export const FETCHING_USER = 'FETCHING_USER'
+export const FETCHING_USER_DONE = 'FETCHING_USER_DONE'
+export const SET_USER = 'SET_USER'
+export const FETCH_USER_ERROR = 'FETCH_USER_ERROR'
+
+const fetchingUser = () => {
   return {
-    type: 'FETCHING_USER',
-    data: bool
+    type: FETCHING_USER
   }
 }
 
 const setUser = (user) => {
   return {
-    type: 'SET_USER',
-    user
+    type: SET_USER,
+    data: user
   }
 }
 
 const fetchUserError = (error) => {
   return {
-    type: 'FETCH_USER_ERROR',
-    error
+    type: FETCH_USER_ERROR,
+    data: error
   }
 }
 
-export const fetchUser = () => {
+export const fetchUser = (id) => {
   return dispatch => {
     // dispatch action we're fetching user
-    dispatch(fetchingUser(true))
+    dispatch(fetchingUser())
     // fetch user from API
-    axios.get('API ENDPOINT')
-    .then(user => setUser(user))
-    .catch(error => fetchUserError(error))
-    // perform finally option
+    axios.get(`/api/users/${id}`)
+    .then(user => dispatch(setUser(user)))
+    .catch(error => {
+      dispatch(fetchUserError(error))
+    })
   }
 }
--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -1,9 +1,27 @@
-const initialState = {}
+import { FETCHING_USER, SET_USER, FETCH_USER_ERROR } from '../actions/user'
+
+const initialState = {user: null, fetching: false, error: null}
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'CHANGE_ME':
-      // return new state
+    case FETCHING_USER:
+      return {
+        ...state,
+        fetching: true,
+        error: null
+      }
+    case SET_USER:
+      return {
+        ...state,
+        user: action.data,
+        fetching: false
+      }
+    case FETCH_USER_ERROR:
+      return {
+        ...state,
+        error: action.data,
+        fetching: false
+      }
     default:
       return state
   }
