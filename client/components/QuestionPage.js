901dd74e4bb391b2705c682f0059bdad112a3512
Innie Kim <innie.jong.kim@gmail.com>
2017-04-14 15:48:16 -0700

    (feat) Add ability to add answer to question (#100)
    - Currently uses an HTTP POST req, but we may need to refactor to use sockets

--- client/components/QuestionPage.js
+++ client/components/QuestionPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AnswerList from './AnswerList'
 import MainQuestion from './MainQuestion'
+import AddAnswer from './AddAnswer'
 
 export default (props) => {
   return (
     <div>
       <MainQuestion id={props.match.params.id} />
       <AnswerList id={props.match.params.id} />
+      <AddAnswer id={props.match.params.id} />
     </div>
   )
 }
--- server/db/models/answers.js
+++ server/db/models/answers.js
@@ -2,7 +2,7 @@ const db = require('../index')
 
 const selectAnswers = (id) => db.manyOrNone('SELECT * FROM answers WHERE user_id = $1', [id])
 
-const selectQuestionAnswers = (id) => db.manyOrNone('SELECT * FROM answers WHERE id = $1', [id])
+const selectQuestionAnswers = (id) => db.manyOrNone('SELECT * FROM answers WHERE question_id = $1', [id])
 
 const insertAnswer = ({ message, user_id, question_id }) => db.none('INSERT INTO answers (message, user_id, question_id) VALUES ($1, $2, $3)', [message, user_id, question_id])
 
