09232c18a52b2fa1519bd0bdd4f47d22826febc4
Innie Kim <innie.jong.kim@gmail.com>
2017-04-17 16:15:48 -0700

    (refactor) DB queries escape special characters using $-quoted string constants

--- server/db/models/questions.js
+++ server/db/models/questions.js
@@ -2,13 +2,13 @@ const db = require('../index')
 
 const selectQuestions = (coordinates) => db.manyOrNone(`SELECT *, ST_AsGeoJSON(coordinates) from questions WHERE ST_DWITHIN(coordinates, ST_SetSRID(ST_MakePoint(${coordinates.lng}, ${coordinates.lat}), 4326)::geography, 402) ORDER BY created_timestamp DESC`)
 
-const selectQuestion = (id) => db.oneOrNone('SELECT *, ST_AsGeoJSON(coordinates) from questions where id = $1', [id])
+const selectQuestion = (id) => db.oneOrNone(`SELECT *, ST_AsGeoJSON(coordinates) from questions where id = ${id}`)
 
-const insertQuestion = ({ user_id, message, coordinates, location, category_id }) => db.none(`INSERT INTO questions (user_id, message, coordinates, location, category_id) VALUES (${user_id}, '${message}', ST_SetSRID(ST_MakePoint(${coordinates.lng}, ${coordinates.lat}), 4326)::geography, '${location}', ${category_id})`)
+const insertQuestion = ({ user_id, message, coordinates, location, category_id }) => db.none(`INSERT INTO questions (user_id, message, coordinates, location, category_id) VALUES (${user_id}, $$${message}$$, ST_SetSRID(ST_MakePoint(${coordinates.lng}, ${coordinates.lat}), 4326)::geography, $$'${location}'$$, ${category_id})`)
 
-const updateQuestion = ({ id, message, category_id }) => db.none('UPDATE questions SET message = $1, category_id = $2 WHERE id = $3', [message, category_id, id])
+const updateQuestion = ({ id, message, category_id }) => db.none(`UPDATE questions SET message = $$${message}$$, category_id = ${category_id} WHERE id = ${id}`)
 
-const deleteQuestion = (id) => db.none('DELETE FROM questions WHERE id = $1', [id])
+const deleteQuestion = (id) => db.none(`DELETE FROM questions WHERE id = ${id}`)
 
 module.exports = {
   selectQuestions: selectQuestions,
