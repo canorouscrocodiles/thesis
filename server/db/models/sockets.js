94801a36577c864945fbcf2847ebcae2f0973646
Innie Kim <innie.jong.kim@gmail.com>
2017-04-17 16:52:51 -0700

    Add socket queries - close #151 (#165)
    * (feat) Add logic to return results within specified distance
    
    * (refactor) Add PostGIS functionality into DB and client
    - Correctly inserts questions into DB
    - Correctly returns questions from DB
    - Client correctly parses coordinates from results for resetView and addPoints
    
    (feat) fetching questions now takes into account user's current location
    
    * (refactor) DB queries escape special characters using $-quoted string constants
    
    * (feat) Add sockets table to db-dev.sql and db-test.sql
    - For upcoming feature to find nearby users to broadcast to
    
    * (feat) Add DB queries to insert, update, select, and delete socketIDs

---
+++ server/db/models/sockets.js
@@ -0,0 +1,16 @@
+const db = require('../index')
+
+const findSockets = (coordinates) => db.manyOrNone(`SELECT * FROM sockets WHERE ST_DWITHIN(coordinates, ST_SetSRID(ST_MakePoint(${coordinates.lng}, ${coordinates.lat}), 4326)::geography, 402)`)
+
+const insertSocket = ({ id, coordinates }) => db.none(`INSERT INTO sockets (id, coordinates) VALUES (${id}, ST_SetSRID(ST_MakePoint(${coordinates.lng}, ${coordinates.lat}), 4326)::geography)`)
+
+const deleteSocket = (id) => db.none(`DELETE FROM sockets WHERE id = ${id}`)
+
+const updateSocket = ({ id, coordinates }) => db.none(`UPDATE sockets SET coordinates = ST_SetSRID(ST_MakePoint(${coordinates.lng}, ${coordinates.lat}), 4326)::geography WHERE id = ${id}`)
+
+module.exports = {
+  findSockets: findSockets,
+  insertSocket: insertSocket,
+  updateSocket: updateSocket,
+  deleteSocket: deleteSocket
+}
