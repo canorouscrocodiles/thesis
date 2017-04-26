c4cf3f5230a38c4ec794e70bd3ac4a4ece3c6278
Keith Alpichi <keithalpichi@gmail.com>
2017-04-25 17:08:45 -0700

    edited webpack and prod scripts- close #193

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,47 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
+var env = process.env.NODE_ENV
+
+let plugins = [ new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }) ]
+let devtool = 'source-map'
+
+if (env === 'production') {
+  devtool = 'cheap-module-source-map'
+  plugins = plugins.concat([
+    new webpack.optimize.UglifyJsPlugin(
+      { beautify: false,
+        mangle: true,
+        compress: true,
+        comments: false
+      }
+    ),
+    new webpack.LoaderOptionsPlugin(
+      {
+        minimize: true,
+        debug: false
+      }
+    )
+  ])
+}
 
 module.exports = {
-  entry: "./client/index.js",
+  entry: './client/index.js',
   output: {
     path: path.resolve(__dirname, 'public/build'),
     filename: 'index.js'
   },
-  devtool: "source-map",
+  devtool: devtool,
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         query: {
-          presets: ["es2015", "react"]
+          presets: ['es2015', 'react']
         }
       }
     ]
   },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development'
-    })
-  ]
-};
+  plugins: plugins
+}
